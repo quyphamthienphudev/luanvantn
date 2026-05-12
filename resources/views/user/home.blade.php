24bd91c9966de4f98dc7fd61ec6610b98b77c7ae
Quy Pham Thien Phu <phamthienphuquy1007@gmail.com>
2026-05-12 09:55:50 +0700

    yeu cau tuan 3

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -5,44 +5,16 @@
 @section('content')
 
 <h1 class="text-2xl font-bold mb-6">
-Danh sách sinh viên tham gia project
+Project luận văn tốt nghiệp
 </h1>
 
 <div class="grid grid-cols-3 gap-6">
 
     <div class="bg-white p-6 rounded-xl shadow">
-        <h3 class="text-gray-500">Phạm Thiên Phú Quý</h3>
         <p class="text-lg font-semibold">
-            % đóng góp: 40%
+            Phạm Thiên Phú Quý
         </p>
-    </div>
-
-    <div class="bg-white p-6 rounded-xl shadow">
-        <h3 class="text-gray-500">Mai Hữu Trí</h3>
-        <p class="text-lg font-semibold">
-            % đóng góp: 15%
-        </p>
-    </div>
-
-    <div class="bg-white p-6 rounded-xl shadow">
-        <h3 class="text-gray-500">Nguyễn Phạm Hoài Nam</h3>
-        <p class="text-lg font-semibold">
-            % đóng góp: 15%
-        </p>
-    </div>
-
-    <div class="bg-white p-6 rounded-xl shadow">
-        <h3 class="text-gray-500">Trương Gia Khánh</h3>
-        <p class="text-lg font-semibold">
-            % đóng góp: 15%
-        </p>
-    </div>
-
-    <div class="bg-white p-6 rounded-xl shadow">
-        <h3 class="text-gray-500">Nguyễn Duy Lê</h3>
-        <p class="text-lg font-semibold">
-            % đóng góp: 15%
-        </p>
+        <h3 class="text-gray-500">DH52201333</h3>
     </div>
 
 </div>
