4b806a48e3f106f40e135c236052b645b4668dc2
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-05-17 05:50:17 +0700

    yeu cau tuan 3

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,59 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kết quả tuần 3</title>
-</head>
-
-<body>
-    <form method="GET" action="/users">
+<form method="GET" action="/users">
     @csrf
-        <h4>Tìm kiếm</h4>
-        <input type="text" name="search" value="{{ $search }}"
-            placeholder="Tìm theo id hoặc theo tên">
-        <select name="sort">
-            <option value="id">Sắp xếp theo ID</option>
-            <option value="name">Sắp xếp theo tên</option>
-        </select>
-        <button>Tìm kiếm</button>
-    </form>
-    <h4>Thêm user</h4>
-    <form method="POST" action="/users/add">
+    <h4>Tìm kiếm</h4>
+    <input type="text" name="search" value="{{ $search }}" placeholder="Tìm theo id hoặc theo tên">
+    <select name="sort">
+        <option value="id">Sắp xếp theo ID</option>
+        <option value="name">Sắp xếp theo tên</option>
+    </select>
+    <button>Tìm kiếm</button>
+</form>
+<h4>Thêm user</h4>
+<form method="POST" action="/users/add">
     @csrf
-        <input type="text" name="id" placeholder="ID">
-        <input type="text" name="name" placeholder="Tên">
-        <input type="text" name="email" placeholder="Email">
-        <input type="text" name="phone" placeholder="Điện thoại">
-        <button>Thêm</button>
-    </form>
-    <br>
-    <!-- success -->
-    @if(session('success'))
-        {{ session('success') }}
-    @endif
-    <!-- error -->
-    @if(session('error'))
-        {{ session('error') }}
-    @endif
-    <!--  -->
-    <br>
-    <br>
-    <table border="1">
-        <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Phone</td>
-            <td>Hành động</td>
-        </tr>
-        @foreach($users as $u)
-        <tr>
-            <td>
-                {{ $u->id }}
-            </td>
-            <form method="POST" action="/users/update/{{ $u->id }}">
+    <input type="text" name="id" placeholder="ID">
+    <input type="text" name="name" placeholder="Tên">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="phone" placeholder="Điện thoại">
+    <button>Thêm</button>
+</form>
+<br>
+<!-- success -->
+@if(session('success'))
+{{ session('success') }}
+@endif
+<!-- error -->
+@if(session('error'))
+{{ session('error') }}
+@endif
+<!--  -->
+<br>
+<br>
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Phone</td>
+        <td>Hành động</td>
+    </tr>
+    @foreach($users as $u)
+    <tr>
+        <td>
+            {{ $u->id }}
+        </td>
+        <form method="POST" action="/users/update/{{ $u->id }}">
             @csrf
             <td>
                 <input type="text" name="name" value="{{ $u->name }}">
@@ -66,14 +55,11 @@
             </td>
             <td>
                 <button>Cập nhật</button>
-                </form>
-                <a href="/users/delete/{{ $u->id }}">
-                    Xóa
-                </a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</body>
-
-</html>
+        </form>
+        <a href="/users/delete/{{ $u->id }}">
+            Xóa
+        </a>
+        </td>
+    </tr>
+    @endforeach
+</table>
