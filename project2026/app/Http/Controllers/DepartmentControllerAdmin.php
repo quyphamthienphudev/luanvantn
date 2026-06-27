c82080e0265a1e34df16c47d1ad708ac08cdfa79
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-06-27 18:27:48 +0700

    cap nhat chuc nang quan ly phong ban

--- project2026/app/Http/Controllers/DepartmentControllerAdmin.php
+++ project2026/app/Http/Controllers/DepartmentControllerAdmin.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Department;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DepartmentControllerAdmin extends Controller
 {
