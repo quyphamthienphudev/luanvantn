b48b595a10d52bfea96aec02136a550c14f7e197
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-07-08 15:47:32 +0700

    bao cao lvtn

--- project2026/app/Http/Middleware/CompanyNetwork.php
+++ project2026/app/Http/Middleware/CompanyNetwork.php
@@ -17,7 +17,7 @@ class CompanyNetwork
     {
         $ip = $request->ip();
 
-        if(!str_starts_with($ip,'192.168.25.'))
+        if(!str_starts_with($ip,'185.27.134.'))
         {
             // abort(403,'Không có quyền truy cập');
             return back();
