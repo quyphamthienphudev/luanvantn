b19073d6c3ac87ce22d656a5db37ec14374a2fe9
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-07-26 15:29:18 +0700

    cap nhat chuc nang bao cao thong ke

--- app/Http/Controllers/HomePageQLCLController.php
+++ app/Http/Controllers/HomePageQLCLController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class HomePageQLCLController extends Controller
 {
     // HOME PAGE
-    public function homePage()
+    public function homePage(Request $request)
     {
         if (auth()->user()->role->name !== 'qlcl') 
         {
             return back();
         }
+        $today = Carbon::now()->today();
+        $month = $request->get('month', date('m'));
         // Số lượng đơn nghỉ phép đã duyệt
         $countLeaveRequestApproved = DB::table('leave_requests')->where('status','approved')->count('users_id');
         // Số lượng đơn nghỉ phép đang chờ
@@ -22,9 +25,9 @@ class HomePageQLCLController extends Controller
         // Số lượng đơn nghỉ phép từ chối
         $countLeaveRequestRejected = DB::table('leave_requests')->where('status','rejected')->count('users_id');
         // Số lượng nhân viên chấm công hôm nay
-        $countAttendanceToday = DB::table('attendances')->where('work_date','2026-06-30')->count('users_id');
+        $countAttendanceToday = DB::table('attendances')->where('work_date', $today)->count('users_id');
         // Số lượng nhân viên chấm công tháng này
-        $countAttendanceForMonth = DB::table('attendances')->distinct('users_id')->whereMonth('work_date','06')->count();
+        $countAttendanceForMonth = DB::table('attendances')->distinct('users_id')->whereMonth('work_date',$month)->count('users_id');
         
         return view('qlcl.home',compact('countLeaveRequestApproved','countLeaveRequestPending','countLeaveRequestRejected','countAttendanceToday','countAttendanceForMonth'));
     }
