0d7ef4a30cd68b39a933dad68ddf48e2c501dc13
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-07-28 15:22:23 +0700

    cap nhat chuc nang cham cong cho nhan vien

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CompanyNetwork;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        $middleware->alias([
-        'company.network'
-            => CompanyNetwork::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
